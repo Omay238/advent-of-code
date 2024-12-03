d1c768944f0adc11a5755d3f025370c574a32cee
Omay <70179858+Omay238@users.noreply.github.com>
2024-12-03 09:01:13 -0700

    part 2 was actually way simpler than what i was trying

--- src/2024-3-2.py
+++ src/2024-3-2.py
@@ -2,19 +2,8 @@ import re
 
 out = 0
 
-dos = list(re.finditer(r"do\(\)", data))
-donts = list(re.finditer(r"don't\(\)", data))
-
 for i in re.finditer(r"mul\((\d{1,3}),(\d{1,3})\)", data):
-    dos_index = len(data) + 1
-    donts_index = 0
-    for j in dos:
-        if j.span()[0] < i.span()[0]:
-            dos_index = j.span()[0]
-    for j in donts:
-        if j.span()[0] < i.span()[0]:
-            donts_index = j.span()[0]
-    if dos_index > donts_index:
+    if data[:i.span()[0]].rfind("do()") >= data[:i.span()[0]].rfind("don't()"):
         out = out + int(i.group(1)) * int(i.group(2))
 
 print(f"{year}-{day}-{part}: {out}")
