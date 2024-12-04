37f8cd2ed4940f6ae2ffd2c2a56008f266a2afb4
Omay <70179858+Omay238@users.noreply.github.com>
2024-12-04 08:42:52 -0700

    day 4

---
+++ src/2024-4-1.py
@@ -0,0 +1,31 @@
+sd = data.split()
+#S  S  S
+# A A A
+#  MMM
+#SAMXMAS
+#  MMM
+# A A A
+#S  S  S
+
+table = [
+    [[1, 0], [2, 0], [3, 0]],
+    [[0, 1], [0, 2], [0, 3]],
+    [[1, 1], [2, 2], [3, 3]],
+    [[-1, 0], [-2, 0], [-3, 0]],
+    [[0, -1], [0, -2], [0, -3]],
+    [[1, -1], [2, -2], [3, -3]],
+    [[-1, 1], [-2, 2], [-3, 3]],
+    [[-1, -1], [-2, -2], [-3, -3]],
+]
+
+out = 0
+
+for i in range(len(sd)):
+    for j in range(len(sd[i])):
+        if sd[i][j] == "X":
+            for k in table:
+                if 0 <= i + k[2][0] < len(sd) and 0 <= j + k[2][1] < len(sd[i]):
+                    if sd[i + k[0][0]][j + k[0][1]] == "M" and sd[i + k[1][0]][j + k[1][1]] == "A" and sd[i + k[2][0]][j + k[2][1]] == "S":
+                        out = out + 1
+
+print (out)
---
+++ src/2024-4-2.py
@@ -0,0 +1,15 @@
+sd = data.split()
+
+out = 0
+
+for i in range(len(sd)):
+    for j in range(len(sd[i])):
+        if sd[i][j] == "A":
+            if 0 < i < len(sd) - 1 and 0 < j < len(sd[i]) - 1:
+                if ((sd[i - 1][j - 1] == "M" and sd[i + 1][j + 1] == "S" or
+                        sd[i - 1][j - 1] == "S" and sd[i + 1][j + 1] == "M") and
+                        (sd[i - 1][j + 1] == "M" and sd[i + 1][j - 1] == "S" or
+                        sd[i - 1][j + 1] == "S" and sd[i + 1][j - 1] == "M")):
+                    out = out + 1
+
+print (out)
